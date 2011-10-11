a624cb32580828a1982e7c4490426304ae9ee6ba
andrew-aladev <aladjev.andrew@gmail.com>
2011-10-11 22:05:36 +0300

    license fix

--- Header.cpp
+++ Header.cpp
@@ -1,17 +1,14 @@
-/*
-This file is part of libfcgipp.
-
+/* This file is part of libfcgipp.
 libfcgipp is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 libfcgipp is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser Public License
+You should have received a copy of the GNU Lesser General Public License
 along with libfcgipp.  If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (C) 2011 Andrew Aladjev <aladjev.andrew@gmail.com>
